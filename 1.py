2818baf78aed8d1ede18f4c206ae301c452308dc
Adrián Kálazi <adrian@kalazi.com>
2023-12-02 01:45:49 +0200

    Clean up 1

--- 1.py
+++ 1.py
@@ -3,40 +3,36 @@ import sys
 
 inp = sys.stdin.read().split()
 
+pat = re.compile(r"(\d)")
+pat2 = re.compile(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))")
 
-def part1():
-    pat = re.compile(r"^\D*(\d).*?(\d)?\D*$")
-    pairs = [re.match(pat, n).groups() for n in inp]
-    nums = [int(n1+n2) if n2 is not None else int(n1+n1) for n1, n2 in pairs]
-    return sum(nums)
+nm = {
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
 
-def part2():
-    pat2 = re.compile(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))")
-    nm = {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4",
-        "5": "5",
-        "6": "6",
-        "7": "7",
-        "8": "8",
-        "9": "9",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-    nums = [int(nm[q[0]] + nm[q[-1]]) for n in inp if (q := pat2.findall(n))]
+def solve(pattern):
+    nums = [int(nm[q[0]] + nm[q[-1]]) for n in inp if (q := pattern.findall(n))]
     return sum(nums)
 
 
 if __name__ == '__main__':
-    print(f"Part 1: {part1()}")
-    print(f"Part 2: {part2()}")
+    print(f"Part 1: {solve(pat)}")
+    print(f"Part 2: {solve(pat2)}")
