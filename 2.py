a089790bfceabd868fbe5371b0e7143080e2c8aa
Adrián Kálazi <adrian@kalazi.com>
2023-12-03 01:39:20 +0200

    Add 2 part 2 + Clean up

--- 2.py
+++ 2.py
@@ -1,6 +1,8 @@
 import re
 import sys
 
+inp = sys.stdin.readlines()
+
 colors = {
     "red": 12,
     "green": 13,
@@ -12,19 +14,20 @@ color_re_template = r"(\d+) {}"
 colors_re = {k: re.compile(color_re_template.format(k)) for k in colors.keys()}
 
 
-def solve_line(line):
+def part1(line: str) -> int:
     for color, color_re in colors_re.items():
         if max(map(int, color_re.findall(line))) > colors[color]:
             return 0
     return int(game_re.search(line).groups()[0])
 
 
-inp = sys.stdin.readlines()
-
-
-def solve():
-    print(sum([solve_line(l) for l in inp]))
+def part2(line: str) -> int:
+    mul = 1
+    for color, color_re in colors_re.items():
+        mul *= max(map(int, color_re.findall(line)))
+    return mul
 
 
 if __name__ == '__main__':
-    solve()
+    print(f"Part 1: {sum(map(part1, inp))}")
+    print(f"Part 2: {sum(map(part2, inp))}")
